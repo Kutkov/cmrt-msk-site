c6670feef33522461fa722d39d3662837ec90fa8
Max <max.k194@gmail.com>
2018-03-13 18:20:03 +0300

    add table style

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,5 @@
 $(function(){
-    // footer add width block
-    $('#footerMenuFirst').css('width', parseInt($('#footerMenuFirst').css('width')) + 20 + 'px');
-    $('#footerMenuSecond').css('width', parseInt($('#footerMenuSecond').css('width')) + 20 + 'px');
+
     // main-menu
     $('.slimmenu').slimmenu(
     {
@@ -12,6 +10,7 @@ $(function(){
         childrenIndenter: '&raquo;',
         easingEffect: null
     });
+    
     // dominance section
     $('.owl-carousel').owlCarousel(
     {
@@ -33,7 +32,11 @@ $(function(){
         }
     });
 
-    // sidebar menu demo
+    // footer add width block
+    $('#footerMenuFirst').css('width', parseInt($('#footerMenuFirst').css('width')) + 20 + 'px');
+    $('#footerMenuSecond').css('width', parseInt($('#footerMenuSecond').css('width')) + 20 + 'px');
+
+    // sidebar menu
     $('.sidebar__tpl-parent > a').after('<i class="fas fa-angle-double-down"></i>');
     $('.sidebar__tpl-parent > i').click(openMenu);
     function openMenu(){
@@ -46,6 +49,21 @@ $(function(){
             $(this).next().css('display', 'block');
             $(this).removeClass("fa-angle-double-down");
             $(this).addClass("fa-angle-double-up");
+        }
+    }
+
+    // table price (click open table)
+    $(".price_table>thead").click(openTable);
+    function openTable(){
+        var arrow = $(this).find('i');
+        if($(this).next().css('display') == 'none'){
+            $(this).next().css('display', 'table-header-group'); 
+            arrow.removeClass('fa-chevron-down');
+            arrow.addClass('fa-chevron-up');
+        }else{
+            $(this).next().css('display', 'none');
+            arrow.removeClass('fa-chevron-up');
+            arrow.addClass('fa-chevron-down');
         }
     }
 
